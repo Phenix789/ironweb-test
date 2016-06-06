083d954d999a68881f663219c5c6f7ef25089249
Claude Ramseyer <claude.ramseyer@gumi-europe.com>
2016-06-06 19:03:11 +0200

    Symfony project

---
+++ app/DoctrineMigrations/Version20160603165232.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160603165232 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL, content LONGTEXT NOT NULL, date DATETIME NOT NULL, INDEX IDX_9474526C7294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE rate (id INT AUTO_INCREMENT NOT NULL, article_id INT DEFAULT NULL, rate SMALLINT NOT NULL, date DATETIME NOT NULL, INDEX IDX_DFEC3F397294869C (article_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE article (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content LONGTEXT NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C7294869C FOREIGN KEY (article_id) REFERENCES article (id)');
+        $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F397294869C FOREIGN KEY (article_id) REFERENCES article (id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C7294869C');
+        $this->addSql('ALTER TABLE rate DROP FOREIGN KEY FK_DFEC3F397294869C');
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE rate');
+        $this->addSql('DROP TABLE article');
+    }
+}
---
+++ tests/IronwebBundle/DataFixtures/LoadData.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace tests\IronwebBundle\DataFixtures;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use IronwebBundle\Entity\Article;
+use IronwebBundle\Entity\Comment;
+use IronwebBundle\Entity\Rate;
+
+/**
+ * Class LoadData
+ *
+ * @package tests\IronwebBundle\DataFixtures
+ *
+ * @author  <ramseyer.claude@gumi-europe.com>
+ */
+class LoadData implements FixtureInterface
+{
+
+    /**
+     * @param ObjectManager $manager
+     *
+     * @author <ramseyer.claude@gumi-europe.com>
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->createFixedArticles($manager);
+        $this->createRandomArticles($manager, 10, 5, 10);
+
+        $manager->flush();
+    }
+
+    /**
+     * @param ObjectManager $manager
+     *
+     * @author <ramseyer.claude@gumi-europe.com>
+     */
+    public function createFixedArticles(ObjectManager $manager)
+    {
+        $article1 = new Article();
+        $article1->setTitle('');
+        $article1->setContent('');
+        $article1->setDate(new \DateTime('-1days'));
+
+        $manager->persist($article1);
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @param int           $nbArticles
+     * @param int           $nbCommentsMax
+     * @param int           $nbRatesMax
+     *
+     * @author <ramseyer.claude@gumi-europe.com>
+     */
+    private function createRandomArticles(ObjectManager $manager, $nbArticles, $nbCommentsMax, $nbRatesMax)
+    {
+        $faker = Factory::create();
+w   
+        $i = 0;
+        while ($i++ < $nbArticles) {
+            //Article
+            $article = new Article();
+            $article->setTitle($faker->title);
+            $article->setContent($faker->text(rand(200, 1000)));
+            $article->setDate($faker->dateTimeThisYear);
+
+            $manager->persist($article);
+
+            //Comment
+            $j        = 0;
+            $comments = rand(0, $nbCommentsMax);
+            while ($j++ < $comments) {
+                $comment = new Comment();
+                $comment->setContent($faker->text());
+                $comment->setDate($faker->dateTimeBetween($article->getDate()));
+                $comment->setArticle($article);
+
+                $manager->persist($comment);
+            }
+
+            //Rate
+            $k     = 0;
+            $rates = rand(0, $nbRatesMax);
+            while ($k++ < $rates) {
+                $rate = new Rate();
+                $rate->setRate($faker->numberBetween(Rate::RATE_MIN, Rate::RATE_MAX));
+                $rate->setDate($faker->dateTimeBetween($article->getDate()));
+                $rate->setArticle($article);
+
+                $manager->persist($rate);
+            }
+        }
+    }
+
+}
